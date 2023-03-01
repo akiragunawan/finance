f2274be4fed1949ab93f09094f7b0820bc3f689b
akira <akiragunawan@gmail.com>
2023-03-01 09:58:25 +0700

    bS

--- client/src/BS/Bs.js
+++ client/src/BS/Bs.js
@@ -19,36 +19,46 @@ function Bs(){
         });
     }, []);
 
-        function data_branch(){
-            for(let i = 0;i< data.branches.count;i++){
-                return console.log(data[i]);
-            }
-        }   
+        // function data_branch(){
+        //     for(let i = 0;i< data.branches.count;i++){
+        //         return console.log(data[i]);
+        //     }
+        // }   
     
     return <div className="container p-5">
         <h2 className="fw-bold text-uppercase ">Balance Sheet</h2>
        <div className="mt-5">
             <div className="card shadow p-3 mb-5 bg-body rounded">
-               <div className="card-body">
-           
-               {data.map(item => (
-                    <div key={item.COA_num}>
-                    <h2>{item.COA_num}</h2>
-                    <div>
                 
-                      {
-                       item.branches.map(cabang =>(
-                        
-                        <div key={cabang.branch_code}>
-                            <p>{cabang.branch_code}</p>
-                            <p>{cabang.branch_name}</p>
+               {data.map((item)=>(
+                <div key={item.COA_num} className="card p-3 m-3 shadow p-3  bg-body rounded">
+                
+                    <div className="d-flex justify-content-between card-title mt-3 ms-4 me-4">
+                        <div className="d-flex flex-column ">
+                            <div className="fw-bold" style={{fontSize:'20px'}}>{item.COA_name}</div>
+                            <div className="text-muted" style={{fontSize:'15px'}}>{item.COA_num}</div> 
+                            
                         </div>
-                       ))
-                      }
+                        <div>
+                            <p className="text-muted">{item.COA_date}</p>
+                        </div>
                     </div>
+                    <div className="d-flex flex-wrap justify-content-start card-body">
+                    {item.Branches.map((branch)=>(
+                    <div className="card p-2 m-2" style={{width:'200px'}}>
+                        <div className="d-flex flex-column">
+                        <div className="fw-bold" style={{fontSize:'15px'}}>{branch.branch_name}</div>
+                          <div className="" style={{fontSize:'10px'}}>{branch.branch_code}</div>                
+                          
+                        </div>
+                        <div style={{fontSize:'20px'}}>
+                            Rp. {branch.value}
+                        </div>
                     </div>
-                ))}
-               </div>
+                    ))}
+                   </div>
+                </div>
+               ))}
             </div>
        </div>
     </div>
