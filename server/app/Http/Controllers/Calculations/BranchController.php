8d13c8fa316bc7f9eb90d5a27e4cbf98d8b190b2
frans.hanjaya <frans.hanjaya@okbank.co.id>
2023-03-17 14:11:50 +0700

    Branches

--- server/app/Http/Controllers/Calculations/BranchController.php
+++ server/app/Http/Controllers/Calculations/BranchController.php
@@ -11,30 +11,12 @@ class BranchController extends Controller
 {
     public function queryBranch(Request $request)
     {   
-        $branches = $this->getBranchNum()->pluck('COLUMN_NAME');
-        $branch = collect();
-        foreach($branches as $b){
-            $branch->push(DB::connection('sqlsrv')
-            ->table('T_Inoan_Branch')
-            ->select('BR_NO as branch_code, ')
-            ->where('BR_NO' ,$b)
-            ->first());
-        }
+        
+        $branches = DB::connection('mysql')
+        ->table('branch')
+        ->select('branch_code','branch_name')
+        ->get();
         return $branches;
     }
     
-    public function getBranchNum(){
-        $branches = DB::connection('sqlsrv')
-        ->table('INFORMATION_SCHEMA.COLUMNS')
-        ->select('COLUMN_NAME')
-        ->where('TABLE_NAME', 'T_Inoan_COAAllBranch')
-        ->get();
-        $branchNum = $branches->filter(function ($value, $key){
-            return is_numeric($value->COLUMN_NAME);
-        });
-        $branchNum->map(function ($value){
-            return (int) $value->COLUMN_NAME;
-        });
-        return $branchNum->values();
-    }
 }
