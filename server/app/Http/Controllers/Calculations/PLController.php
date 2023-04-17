813b20940d3d387adcd890e8e3d837364fa2817c
frans.hanjaya <frans.hanjaya@okbank.co.id>
2023-04-17 13:56:56 +0700

    fix DB PL

--- server/app/Http/Controllers/Calculations/PLController.php
+++ server/app/Http/Controllers/Calculations/PLController.php
@@ -16,8 +16,8 @@ class PLController extends Controller
         $date = Carbon::createFromDate($year, $month, Carbon::create($year,$month)->endOfMonth()->day)->toDateString();
         
         $result = array();
-        $branches = explode(',', config('auth.branches'));
-        $coas = explode(',', config('auth.pl_coa'));
+        $branches = DB::table('branch')->select('*')->get();
+        $coas = DB::table('coa')->select('*')->get()->pluck('coa');
         foreach($coas as $c){
             $res = DB::connection('sqlsrv')->
             table('T_Inoan_COAAllBranch')
@@ -33,18 +33,18 @@ class PLController extends Controller
             
             $branch_array = array();
             $b_array = array();
-            $b_data['branch_name'] = explode(',', config('auth.branches_name'));
-            $b_data['branch_code'] = explode(',', config('auth.branches'));
+            $b_data['branch_name'] = $branches->pluck('branch_name');
+            $b_data['branch_code'] = $branches->pluck('branch_code');
             $i = 0;
             foreach($branches as $b){
                 $b_array['branch_name'] = $b_data['branch_name'][$i];
                 $b_array['branch_code'] = $b_data['branch_code'][$i];
                 if($b_array['branch_code']=='1108'){
-                    $temp = $this->getPLPerBranch($date, $c, $b)->first();
+                    $temp = $this->getPLPerBranch($date, $c, '1108')->first();
                     $temp = collect($temp);
                     $b_array['value'] = number_format((float)$temp->get('IDRBalance')/1000000);
                 }
-                else $b_array['value'] = number_format((float)($res->get($b))/1000000);
+                else $b_array['value'] = number_format((float)($res[$b->branch_code])/1000000);
                 array_push($branch_array, $b_array);
                 $i++;
             }
