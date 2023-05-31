bfa09eb41320cb69374e74a87b969ef11ab7c144
frans.hanjaya <frans.hanjaya@okbank.co.id>
2023-05-31 14:35:57 +0700

    bs pl bandung

--- server/app/Http/Controllers/Calculations/BSController.php
+++ server/app/Http/Controllers/Calculations/BSController.php
@@ -55,7 +55,10 @@ class BSController extends Controller
                         $b_array['value'] = number_format((float)$temp->get('IDRBalance')/1000000);
                         
                     }
-                    else $b_array['value'] = number_format((float)($res[$b->branch_code])/1000000);
+                    else if(@$res[$b->branch_code]){
+                        $b_array['value'] = number_format((float)($res[$b->branch_code])/1000000);
+                    }
+                    else $b_array['value'] = "0";
                     
                     array_push($branch_array, $b_array);
                     $i++;
