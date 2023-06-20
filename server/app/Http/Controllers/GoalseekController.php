ee81563599be59de7ba1af078919876371ce1d2a
frans.hanjaya <frans.hanjaya@okbank.co.id>
2023-06-20 09:58:35 +0700

    bandung  fix

--- server/app/Http/Controllers/GoalseekController.php
+++ server/app/Http/Controllers/GoalseekController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\PHPExcelGoalSeek;
 
 class GoalSeekController extends PHPExcelGoalSeek{
-
+    private $var = 0;
+    private $arr = array();
     function callbackTest($input, $rate, $IC, $PIO, $S, $C, $CKPN_Prev, $month) {
         $balance = $input;
         $TI = ($balance* $rate * $month /12) + $C + $PIO;
@@ -12,6 +13,10 @@ class GoalSeekController extends PHPExcelGoalSeek{
         $OC = $S + $CKPN;
         $TC = $OC + $IC;
         $profit = $TI - $TC;
+        //1.878019710000217
+        $this->var++;
+        array_push($this->arr, $profit);
+        // if($this->var == 31) dd($IC, $OC, $TC, $profit);
         return $profit;
     }
 }
